bb6896d82664074dc51793339b313aae4ef152fa
anomalchik <vidslava@yandex.ru>
2019-03-08 08:03:36 -0100

    sound: soc: add sound changes for X00T added aw87339(maybe not used) added tfa9874 q6afe hotfixes

--- sound/soc/msm/sdm660-common.c
+++ sound/soc/msm/sdm660-common.c
@@ -30,6 +30,10 @@
 #define DEV_NAME_STR_LEN  32
 #define DEFAULT_MCLK_RATE 9600000
 
+#ifdef CONFIG_INPUT_SX9310
+extern void sar_switch(bool);
+#endif
+
 struct dev_config {
 	u32 sample_rate;
 	u32 bit_format;
@@ -200,9 +204,15 @@ static struct wcd_mbhc_config mbhc_cfg = {
 	.swap_gnd_mic = NULL,
 	.hs_ext_micbias = true,
 	.key_code[0] = KEY_MEDIA,
+#ifdef CONFIG_MACH_ASUS_X00T
+	.key_code[1] = KEY_VOLUMEUP,
+	.key_code[2] = KEY_VOLUMEDOWN,
+	.key_code[3] = 0,
+#else
 	.key_code[1] = KEY_VOICECOMMAND,
 	.key_code[2] = KEY_VOLUMEUP,
 	.key_code[3] = KEY_VOLUMEDOWN,
+#endif
 	.key_code[4] = 0,
 	.key_code[5] = 0,
 	.key_code[6] = 0,
@@ -231,7 +241,11 @@ static struct dev_config mi2s_rx_cfg[] = {
 static struct dev_config mi2s_tx_cfg[] = {
 	[PRIM_MI2S] = {SAMPLING_RATE_48KHZ, SNDRV_PCM_FORMAT_S16_LE, 1},
 	[SEC_MI2S]  = {SAMPLING_RATE_48KHZ, SNDRV_PCM_FORMAT_S16_LE, 1},
+#ifdef CONFIG_MACH_ASUS_X00T
+	[TERT_MI2S] = {SAMPLING_RATE_48KHZ, SNDRV_PCM_FORMAT_S16_LE, 2},
+#else
 	[TERT_MI2S] = {SAMPLING_RATE_48KHZ, SNDRV_PCM_FORMAT_S16_LE, 1},
+#endif
 	[QUAT_MI2S] = {SAMPLING_RATE_48KHZ, SNDRV_PCM_FORMAT_S16_LE, 1},
 };
 
@@ -2465,6 +2479,10 @@ int msm_mi2s_snd_startup(struct snd_pcm_substream *substream)
 	int index = cpu_dai->id;
 	unsigned int fmt = SND_SOC_DAIFMT_CBS_CFS;
 
+#ifdef CONFIG_MACH_ASUS_X00T
+	struct msm_asoc_mach_data *pdata = snd_soc_card_get_drvdata(rtd->card);
+#endif
+
 	dev_dbg(rtd->card->dev,
 		"%s: substream = %s  stream = %d, dai name %s, dai ID %d\n",
 		__func__, substream->name, substream->stream,
@@ -2515,6 +2533,18 @@ int msm_mi2s_snd_startup(struct snd_pcm_substream *substream)
 				goto clk_off;
 			}
 		}
+#ifdef CONFIG_MACH_ASUS_X00T
+		if (index == TERT_MI2S) {
+			/* Huaqin add sar switcher by chenyijun5 at 2018/03/20 start*/
+			#ifdef CONFIG_INPUT_SX9310
+			pr_debug("%s before open PA, close SAR!\n", __func__);
+			sar_switch(0);
+			#endif
+			/* Huaqin add sar switcher by chenyijun5 at 2018/03/20 end*/
+		    msm_cdc_pinctrl_select_active_state(pdata->tert_mi2s_gpio_p);
+			printk("daixianze %s tert_mi2s_gpio_p\n", __func__);
+		}
+#endif
 	}
 	mutex_unlock(&mi2s_intf_conf[index].lock);
 	return 0;
@@ -2542,6 +2572,10 @@ void msm_mi2s_snd_shutdown(struct snd_pcm_substream *substream)
 	int port_id = msm_get_port_id(rtd->dai_link->be_id);
 	int index = rtd->cpu_dai->id;
 
+#ifdef CONFIG_MACH_ASUS_X00T
+	struct msm_asoc_mach_data *pdata = snd_soc_card_get_drvdata(rtd->card);
+#endif
+
 	pr_debug("%s(): substream = %s  stream = %d\n", __func__,
 		 substream->name, substream->stream);
 	if (index < PRIM_MI2S || index > QUAT_MI2S) {
@@ -2551,10 +2585,30 @@ void msm_mi2s_snd_shutdown(struct snd_pcm_substream *substream)
 
 	mutex_lock(&mi2s_intf_conf[index].lock);
 	if (--mi2s_intf_conf[index].ref_cnt == 0) {
+#ifdef CONFIG_MACH_ASUS_X00T
+		if (index == TERT_MI2S)
+		{
+		    msm_cdc_pinctrl_select_sleep_state(pdata->tert_mi2s_gpio_p);
+			pr_err("daixianze %s tert_mi2s_gpio_p \n", __func__);
+			/* Huaqin add sar switcher by chenyijun5 at 2018/03/20 start*/
+			#ifdef CONFIG_INPUT_SX9310
+			pr_debug("%s after close PA, open SAR!\n", __func__);
+			sar_switch(1);
+			#endif
+			/* Huaqin add sar switcher by chenyijun5 at 2018/03/20 end*/
+		}
+#endif
 		ret = msm_mi2s_set_sclk(substream, false);
 		if (ret < 0)
+#ifdef CONFIG_MACH_ASUS_X00T
+		{
+#endif
 			pr_err("%s:clock disable failed for MI2S (%d); ret=%d\n",
 				__func__, index, ret);
+#ifdef CONFIG_MACH_ASUS_X00T
+			mi2s_intf_conf[index].ref_cnt++;
+		}
+#endif
 		if (mi2s_intf_conf[index].msm_is_ext_mclk) {
 			mi2s_mclk[index].enable = 0;
 			pr_debug("%s: Disabling mclk, clk_freq_in_hz = %u\n",
@@ -3084,6 +3138,10 @@ static int msm_asoc_machine_probe(struct platform_device *pdev)
 					"qcom,cdc-dmic-gpios", 0);
 		pdata->ext_spk_gpio_p = of_parse_phandle(pdev->dev.of_node,
 					"qcom,cdc-ext-spk-gpios", 0);
+#ifdef CONFIG_MACH_ASUS_X00T
+		pdata->tert_mi2s_gpio_p = of_parse_phandle(pdev->dev.of_node,
+				    "qcom,tert-mi2s-gpios", 0);
+#endif
 	}
 
 	/*
